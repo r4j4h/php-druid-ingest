e6e9cea5648ecf6702342550790c7ca31fbca126
Jasmine Hegman <jasmine@webpt.com>
2014-09-23 12:17:43 -0700

    Mocking writeFile so that test does not actually cause IO

--- tests/Preparer/LocalPhpArrayToJsonPreparerTest.php
+++ tests/Preparer/LocalPhpArrayToJsonPreparerTest.php
@@ -8,13 +8,22 @@ class LocalPhpArrayToJsonPreparerTest extends PHPUnit_Framework_TestCase
 {
     public function testWritesWithNewlineDelimiter()
     {
-        $p = new LocalPhpArrayToJsonFilePreparer();
+        $p = $this->getMock('PhpDruidIngest\Preparer\LocalPhpArrayToJsonFilePreparer', array('writeFile'));
+
+        $expectedJsonResponse = '{"id":1,"animal":"cat","legs":4}' .
+            "\n" .
+            '{"id":2,"animal":"cat","legs":3}' .
+            "\n" .
+            '{"id":3,"animal":"dog","legs":4}'
+        ;
+
+        $p->expects($this->once())->method('writeFile')->with( $this->anything(), $expectedJsonResponse );
+
 
         $fakeData = array(
             array( 'id' => 1,   'animal' => 'cat',    'legs' => 4 ),
             array( 'id' => 2,   'animal' => 'cat',    'legs' => 3 ),
             array( 'id' => 3,   'animal' => 'dog',    'legs' => 4 ),
-            array( 'id' => 4,   'animal' => 'spider', 'legs' => 8 ),
         );
 
         $p->prepare( $fakeData );
